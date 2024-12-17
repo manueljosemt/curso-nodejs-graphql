044d2246c52c12507380aeff253da471ef01ae3a
Manuel Michelangelli <mmichelangellit@ripley.com>
2024-12-17 17:12:10 -0300

    My first query resolvers

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -3,13 +3,47 @@ const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-serve
 
 const typeDefs = `
   type Query {
-    hello: String
+    hello: String!
+    getPerson(name: String, age: Int): String
+    getInt(age: Int!): Int
+    getFloat: Float
+    getString: String
+    getBoolean: Boolean
+    getId: ID
+    getNumbers(numbers: [Int!]!): [Int]
+    getProduct: Product
+  }
+
+  type Product {
+    id: ID!
+    name: String!
+    price: Float!
+    description: String!
+    image: String!
+    createdAt: String!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'hola mundo'
+    hello: () => 'hola mundo',
+    getPerson: (_, args) => `Hello my name is ${args.name}, I'm ${args.age} years old`,
+    getInt: (_, args) => args.age,
+    getFloat: () => 1.1,
+    getString: () => 'palabra',
+    getBoolean: () => true,
+    getId: () => 12341234,
+    getNumbers: (_, args) => args.numbers,
+    getProduct: () => {
+      return {
+        id: '12341234',
+        name: 'product1',
+        price: 100.12,
+        description: 'test',
+        image: 'http://image.asas',
+        createdAt: new Date().toISOString()
+      }
+    }
   }
 }
 
