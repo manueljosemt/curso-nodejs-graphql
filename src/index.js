b200e47634289bd97d29c50f7b4dfaf7ef0a0f78
Manuel Michelangelli <mmichelangellit@ripley.com>
2024-12-17 15:56:20 -0300

    My first graphql server and endpoint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const createApp = require('./app');
 
-const port = process.env.PORT || 3000;
-const app = createApp();
-app.listen(port, () => {
-  console.log(`Mi port ${port}`);
-});
+(async () => {
+  const port = process.env.PORT || 3000;
+  const app = await createApp();
+  app.listen(port, () => {
+    console.log(`Mi port ${port}`);
+  });
+})();
+
