0fb7cc9f462ae8077dc5222a8636c3699e0dae2c
Manuel Michelangelli <mmichelangellit@ripley.com>
2024-12-17 20:59:38 -0300

    Check roles

--- src/graphql/category.resolvers.js
+++ src/graphql/category.resolvers.js
@@ -1,9 +1,12 @@
 const boom = require('@hapi/boom');
+const checkRolesGql = require('../utils/checkRolesGql')
 const CategoryService = require('./../services/category.service');
 const service = new CategoryService()
 
 const addCategory = async (_, {dto}, context) => {
   const { user } = await context.authenticate('jwt', {session: false})
+
+  checkRolesGql(user, 'admin')
 
   if (!user) {
     throw boom.unauthorized('jwt is not valid')
